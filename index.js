66187750fcdb98d44e3885c34be0a46b3cc3b12a
Emilio <marqetm@gmail.com>
2022-05-25 20:52:27 -0500

    commit tree a6d19df9b4a2c0bd86dd73d830cb5b44e5be2a3b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, holiday) {
+  const message = [];
+  for(let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+  };
+  console.log(message);
+  return message;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(numb) {
+  while(numb >= 0) {
+    console.log(numb--);
+  }
+}
+
+countDown(10);
